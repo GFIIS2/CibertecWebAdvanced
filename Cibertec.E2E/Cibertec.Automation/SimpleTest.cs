e04ef6c59030242afe9753197ca5668770cb721b
Gustavo Yauri <gryauri@gmail.com>
2017-06-22 22:24:42 -0500

    Solucion de Automatizacion de pruebas

---
+++ Cibertec.E2E/Cibertec.Automation/SimpleTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Cibertec.Automation
+{
+    public static class SimpleTest
+    {
+        public static void Navigate()
+        {
+            //NOTA: La URL inicia con HTTP://
+            var driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("http://www.google.com.pe");
+            driver.Navigate().GoToUrl("http://localhost:5000/Customer"); 
+
+            //NOTA: Con este comando cierro sino lo dejo abierto
+            //driver.Close();
+        }
+    }
+}
